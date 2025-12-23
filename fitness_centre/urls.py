9000ce16d16591d005031c26d05f15de368a1640
Viggnesh Naik <naikviggnesh@gmail.com>
2025-12-23 11:44:25 +0530

    Add Django Prometheus URL

--- fitness_centre/urls.py
+++ fitness_centre/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("core/", include("fitness_core.urls")),
+    path('', include('django_prometheus.urls')),
 ]
